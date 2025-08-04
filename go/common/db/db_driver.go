82aa47bc8b7453c4e2f67241606e9f437952f622
Hanasou <royzhang42@gmail.com>
2025-08-03 17:07:24 -0700

    Remove legacy db driver

--- go/common/db/db_driver.go
+++ go/common/db/db_driver.go
@@ -2,12 +2,6 @@ package db
 
 import "github.com/Hanasou/news_feed/go/common"
 
-// Common interface database drivers can implement
-type LegacyDbDriver interface {
-	Upsert(common.Serializable) error
-	GetData() ([]common.Serializable, error)
-}
-
 type DbDriver[T common.Serializable] interface {
 	Upsert(T) error
 	GetData() ([]T, error)
